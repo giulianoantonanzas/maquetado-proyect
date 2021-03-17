403f0975d283ab43a89aa707cae6e9343de8676e
DESKTOP-8NK97U4\patyg <giulianoantonanzas@gmail.com>
2021-03-17 10:03:10 -0300

    footer + full screen masthead
    Implementacion del footer y actualizacion del masthead en donde se agrega el auto full screen con un script

---
+++ assets/script/fullScreenHeight.js
@@ -0,0 +1,6 @@
+var masthead= document.getElementById('masthead')
+//el alto de la pantalla -20% que es del navbar
+const dimension=(window.screen.availHeight-window.screen.availHeight *20/100)+'px'
+
+masthead.style.height=dimension
+
