47789b85031b42a0b2d55fd4761519c7fac0547c
DESKTOP-8NK97U4\patyg <giulianoantonanzas@gmail.com>
2021-03-17 11:46:02 -0300

    Agregando box one box two

--- assets/script/fullScreenHeight.js
+++ assets/script/fullScreenHeight.js
@@ -1,6 +1,5 @@
 var masthead= document.getElementById('masthead')
-//el alto de la pantalla -20% que es del navbar
-const dimension=(window.screen.availHeight-window.screen.availHeight *20/100)+'px'
+//el alto de la pantalla -20% que es del navbar y otros elementos de la pantall
+const dimension=(window.screen.height-window.screen.height *20/100)+'px'
 
 masthead.style.height=dimension
-
