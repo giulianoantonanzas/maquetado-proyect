9854db4f8ca71886a86830635446cf2699b3aae5
DESKTOP-8NK97U4\patyg <giulianoantonanzas@gmail.com>
2021-03-23 10:05:12 -0300

    Detalles del diseño integrados
    Comenzando con los demas elementos.

--- masthead/fullScreenHeight.js
+++
@@ -1,5 +0,0 @@
-var masthead= document.getElementById('masthead')
-//el alto de la pantalla -20% que es del navbar y otros elementos de la pantall
-const dimension=(window.screen.height-window.screen.height *20/100)+'px'
-
-masthead.style.height=dimension
